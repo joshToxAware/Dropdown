2f9d01cafaaae126760ad5db88cb67474787beb5
joshToxAware <joshcallander@toxawaresoftware.com>
2022-01-13 11:20:07 +1300

    2 files modified

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var currentCaseId = 2215;
 window.ddPatientsList = [
   {
     Name: 'John Smith',
-    CaseId: 'W2264',
+    CaseId: 'W2264&nbsp;&bull;&nbsp;',
     Lock: '',
-    PrimarySubstance: 'Aspirin',
+    PrimarySubstance: 'Aspirin&nbsp;&bull;&nbsp;',
     Age: '14 years old',
     Image:
       'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png',
@@ -14,9 +14,9 @@ window.ddPatientsList = [
   },
   {
     Name: 'Bryan Smith',
-    CaseId: 'W2265',
-    Lock: '<img class="lockImage" src="https://dev.toxaware.net/assets/Images/Padlock_Gold.png" alt="Call Locked"></div>',
-    PrimarySubstance: 'Beer',
+    CaseId: 'W2265&nbsp;&bull;&nbsp;',
+    Lock: '<img src="https://dev.toxaware.net/assets/Images/Padlock_Gold.png" alt="Call Locked" class="lockImage">',
+    PrimarySubstance: 'Beer&nbsp;&bull;&nbsp;',
     Age: '42 years old',
     Image:
       'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png',
@@ -38,7 +38,7 @@ window.ddPatientsList = [
     PrimarySubstance: '',
     Age: '',
     Image: 'https://www.pngrepo.com/png/238362/512/right-arrow-next.png',
-    URL: 'https://dev.toxaware.net/exposure/edit/'+(currentCaseId+1),
+    URL: 'https://dev.toxaware.net/exposure/edit/' + (currentCaseId + 1),
   },
   {
     Name: 'Previous patient',
@@ -47,7 +47,7 @@ window.ddPatientsList = [
     PrimarySubstance: '',
     Age: '',
     Image: 'http://cdn.onlinewebfonts.com/svg/img_529.png',
-    URL: 'https://dev.toxaware.net/exposure/edit/'+(currentCaseId-1),
+    URL: 'https://dev.toxaware.net/exposure/edit/' + (currentCaseId - 1),
   },
 ];
 // initialize DropDownList component
@@ -56,17 +56,12 @@ var dropDownListObj = new ej.dropdowns.DropDownList({
   dataSource: window.ddPatientsList,
   // map the appropriate columns to fields property
   fields: { text: 'Name' },
-  // set the template content for popup header element
-  headerTemplate:
-    '<div class="header"> <span>Photo</span> <span class="info">Patient Info</span></div>',
   // set the template content for list items
   itemTemplate:
-    '<div class="itemWrapper"><img class="patientImage" src="${Image}" alt="${Name}"><div class="patientInfoWrapper"><div class="patientName">${Name}</div><div class="caseId">${CaseId}&nbsp;&bull;&nbsp;${PrimarySubstance}&nbsp;&bull;&nbsp;${Age}</div></div>${Lock}',
+    '<div class="patientWrapper"><img src="${Image}" alt="${Name}" class="patientImage"><div class="patientInfoWrapper"><div class="patientName">${Name}</div><div class="patientInfo">${CaseId} ${PrimarySubstance} ${Age}</div></div>${Lock}</div>',
   // set the template content for displays the selected items in input element.
   valueTemplate:
-    '<div class="itemSelected">' +
-    '<img class="value" src=" ${Image} " height="26px" width="26px" alt="Patient"/>' +
-    '<div class="name"> ${Name} </div></div>',
+    '<div class="itemSelected"><img class="itemSelectedImage" src="${Image}" height="26px" width="26px" alt="${Name}"><span class="itemSelectedName">${Name}</span></div>',
   // set the placeholder to DropDownList input element
   placeholder: 'Switch between patients',
   // set the height of the popup element
