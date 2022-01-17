a13b636573cc2cf8b7df28613f78012b35fcbbbd
joshToxAware <joshcallander@toxawaresoftware.com>
2022-01-18 11:06:51 +1300

    2 files modified

--- index.js
+++ index.js
@@ -1,58 +1,60 @@
-var currentCaseId = 2215;
+// these need to be changed 
+var currentExposureId = 2214;
 
-// The data array. Note that images are just placeholders
+// The data array. Note that images are just placeholders. Order by CaseId
 window.ddPatientsList = [
   {
-    Name: 'Smith, John',
-    CaseId: 'W2264&nbsp;&nbsp;&bull;&nbsp;&nbsp;',
-    Lock: '',
-    PrimarySubstance: 'Aspirin&nbsp;&nbsp;&bull;&nbsp;&nbsp;',
-    Age: 14,
-    AgeUnit: 'Years',
-    Image:
-      'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png',
-    URL: '#'
+    caseId: "Z2212",
+    ctrId: 0,
+    exposureId: 2212,
+    exposureLock: false,
+    exposureStatus: "Open",
+    patientAge: "42 Years",
+    patientGender: "Male",
+    patientName: "James, Liam",
+    substanceName: "Pizza Hut pepperoni pizza",
+
+    image: 'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png'
   },
   {
-    Name: 'Smith, Bryan',
-    CaseId: 'W2265&nbsp;&nbsp;&bull;&nbsp;&nbsp;',
-    Lock: '<img src="https://dev.toxaware.net/assets/Images/Padlock_Gold.png" alt="Call Locked" class="lockImage">',
-    PrimarySubstance: 'Beer&nbsp;&nbsp;&bull;&nbsp;&nbsp;',
-    Age: 42,
-    AgeUnit: 'Years',
-    Image:
-      'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png',
-    URL: '#'
+    caseId: "Z2214",
+    ctrId: 0,
+    exposureId: 2214,
+    exposureLock: false,
+    exposureStatus: "Open",
+    patientAge: "13 Years",
+    patientGender: "Female",
+    patientName: "Young, Grace",
+    substanceName: "Pizza Hut pepperoni pizza",
+
+    image: 'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png'
   },
   {
-    Name: 'Add patient',
-    CaseId: '',
-    Lock: '',
-    PrimarySubstance: '',
-    Age: '',
-    AgeUnit: '',
-    Image: 'https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/plus-512.png',
-    URL: '#'
+    caseId: "Z2215",
+    ctrId: 0,
+    exposureId: 2215,
+    exposureLock: false,
+    exposureStatus: "Open",
+    patientAge: "13 Years",
+    patientGender: "Male",
+    patientName: "Fell, Joe",
+    substanceName: "Pizza Hut pepperoni pizza",
+
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png'
   },
   {
-    Name: 'Next patient',
-    CaseId: 'W2286',
-    Lock: '',
-    PrimarySubstance: '',
-    Age: '',
-    AgeUnit: '',
-    Image: 'https://www.pngrepo.com/png/238362/512/right-arrow-next.png',
-    URL: 'https://dev.toxaware.net/exposure/edit/' + (currentCaseId + 1)
-  },
-  {
-    Name: 'Previous patient',
-    CaseId: 'W2284',
-    Lock: '',
-    PrimarySubstance: '',
-    Age: '',
-    AgeUnit: '',
-    Image: 'http://cdn.onlinewebfonts.com/svg/img_529.png',
-    URL: 'https://dev.toxaware.net/exposure/edit/' + (currentCaseId - 1)
+    caseId: "Z2216",
+    ctrId: 0,
+    exposureId: 2216,
+    exposureLock: true,
+    exposureStatus: "Open",
+    patientAge: "14 Years",
+    patientGender: "Male",
+    patientName: "Hornee, Charlie",
+    substanceName: "Beer",
+
+    image: 'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png'
   }
 ];
 
@@ -61,21 +63,65 @@ var dropDownListObj = new ej.dropdowns.DropDownList({
   // set the patients data to dataSource property
   dataSource: window.ddPatientsList,
   // map the appropriate columns to fields property
-  fields: { text: 'Name' },
+  fields: { text: 'Name', value: 'caseId' },
   // set the template content for list items
   itemTemplate:
-    '<div class="patientWrapper"><img src="${Image}" alt="${Name}" class="patientImage"><div class="patientInfoWrapper"><div class="patientName">${Name}</div><div class="patientInfo">${CaseId}${PrimarySubstance}${Age} ${AgeUnit}</div></div>${Lock}</div>',
+    '<div class="patientWrapper"><img src="${image}" alt="${patientName}" class="patientImage"><div class="patientInfoWrapper"><div class="patientName">${patientName}</div><div class="patientInfo">${caseId} ${substanceName} ${patientAge}</div></div><img src="https://dev.toxaware.net/assets/Images/Padlock_Gold.png" alt="Expousre Lock Status: ${exposureLock}" class="${exposureLock}LockImage"></div>',
   // set the template content for displays the selected items in input element.
   valueTemplate:
-    '<div class="itemSelected"><img class="itemSelectedImage" src="${Image}" height="26px" width="26px" alt="${Name}"><span class="itemSelectedName">${Name}</span></div>',
+    '<div class="itemSelected"><img class="itemSelectedImage" src="${image}" height="26px" width="26px" alt="${patientName}"><span class="itemSelectedName">${patientName}</span></div>',
   // set the placeholder to DropDownList input element
   placeholder: 'Switch between patients',
   // set the height of the popup element
   popupHeight: '300px',
-  // bind the change event
-  change: function() {
-    // somehow redirect to the appropriate URL
+  // the change event
+  change: (event) => {
+    // do any nessecary validation before redirecing. DR mentioned that 
+    if (1 == 2) {
+      // add error.
+    }
+
+    else {
+      // redirect the user to the approptiate case
+      window.location.assign("/exposure/edit/" + event.itemData.caseId.substring(1));
+    }
   }
 });
 
 dropDownListObj.appendTo('#patients');
+
+// logic for if the next paitent button is needed
+if (currentExposureId < window.ddPatientsList[window.ddPatientsList.length - 1].exposureId) {
+  // fetch this from database
+  dropDownListObj.addItem({
+    caseId: "Z2214",
+    ctrId: 0,
+    exposureId: 2214,
+    exposureLock: false,
+    exposureStatus: "Open",
+    patientAge: "",
+    patientGender: "",
+    patientName: "Next paitent",
+    substanceName: "",
+
+    image: 'https://www.pngrepo.com/png/238362/512/right-arrow-next.png'
+  });
+}
+
+// logic for if the previous paitent button is needed
+if (currentExposureId > window.ddPatientsList[0].exposureId) {
+  // fetch this from database
+  dropDownListObj.addItem({
+    caseId: "Z2212",
+    ctrId: 0,
+    exposureId: 2214,
+    exposureLock: false,
+    exposureStatus: "Open",
+    patientAge: "",
+    patientGender: "",
+    patientName: "Previous paitent",
+    substanceName: "",
+
+    image: 'https://www.pngrepo.com/png/238362/512/right-arrow-next.png'
+  });
+}
